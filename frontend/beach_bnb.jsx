26e52db09a63260d459f8f1176fbe9a0942540c0
Clare Hsu <clare.hsu390@gmail.com>
2017-07-14 14:20:27 -0700

    Set up front-end authentication

---
+++ frontend/beach_bnb.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { login, logout, signup} from './util/session_api_util';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>Welcome to BeachBnb</h1>, root);
+  window.signup = signup;
+});
