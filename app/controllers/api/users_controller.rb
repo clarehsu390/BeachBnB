a2cc15ff60f94b37d50902d8336cae47703fac87
Clare Hsu <clare.hsu390@gmail.com>
2017-07-17 17:18:01 -0700

    Added Auth routes

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,14 +1,13 @@
 class Api::UsersController < ApplicationController
   def create
   @user = User.new(user_params)
-  p @user
-  if @user.save
-    login(@user)
-    render "api/users/show"
-  else
-    render json: @user.errors.full_messages, status: 422
+    if @user.save
+      login(@user)
+      render "api/users/show"
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
   end
-end
 
 private
 
--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import GreetingContainer from './greeting/greeting_container';
 import SessionFormContainer from './session/session_form_container';
 import { Route, Switch } from 'react-router-dom';
+import { AuthRoute } from '../util/route_util';
 const App = () => (
   <div>
     <header>
@@ -9,8 +10,8 @@ const App = () => (
     <GreetingContainer />
     </header>
     <Switch>
-      <Route path="/login" component={SessionFormContainer} />
-      <Route path="/signup" component={SessionFormContainer} />
+      <AuthRoute path="/login" component={SessionFormContainer} />
+      <AuthRoute path="/signup" component={SessionFormContainer} />
     </Switch>
   </div>
 );
